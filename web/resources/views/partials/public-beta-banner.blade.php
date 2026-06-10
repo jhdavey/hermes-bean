949f84f3b5452d8ba3886670b6fdb139968f344c
Harley Davey <jhdavey5182@gmail.com>
2026-06-09 22:24:50 -0400

    Add public beta banner

---
+++ web/resources/views/partials/public-beta-banner.blade.php
@@ -0,0 +1,5 @@
+<div class="public-beta-banner" role="note">
+    <div class="public-beta-banner-inner">
+        <span><strong>HeyBean is currently in beta.</strong> If you have any issues, <a href="/support">please report them here</a>.</span>
+    </div>
+</div>
