66876d3a6e2171b35b245ea9e1b7f0b650bfc838
Harley Davey <jhdavey5182@gmail.com>
2026-06-18 13:50:13 -0400

    Refresh public pricing pages

--- web/resources/views/partials/public-nav.blade.php
+++ web/resources/views/partials/public-nav.blade.php
@@ -1,20 +1,19 @@
 <header class="wrap nav">
     <a class="brand" href="/"><img src="{{ asset('images/bean-logo.png') }}" alt="HeyBean logo">HeyBean</a>
     <nav class="navlinks" aria-label="Primary navigation">
-        <a href="/">Home</a>
-        <a href="/#how">How it works</a>
-        <a href="/#features">Features</a>
         <a href="/pricing">Pricing</a>
-        <a href="/login">Log in</a>
+        <a href="/#reviews">Reviews</a>
+        <a href="/#features">Features</a>
+        <a class="nav-login" href="/login">Login</a>
     </nav>
     <details class="mobile-menu">
         <summary aria-label="Open menu"><span class="mobile-menu-icon" aria-hidden="true"><span></span><span></span><span></span></span></summary>
         <div class="mobile-menu-panel">
             <a href="/">Home</a>
-            <a href="/#how">How it works</a>
-            <a href="/#features">Features</a>
             <a href="/pricing">Pricing</a>
-            <a href="/login">Log in</a>
+            <a href="/#reviews">Reviews</a>
+            <a href="/#features">Features</a>
+            <a href="/login">Login</a>
         </div>
     </details>
 </header>
